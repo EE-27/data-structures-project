9f1ca38a8ea998efce2dd51436a2a74323ba2935
JL34 <johnny2791@seznam.cz>
2023-09-19 14:06:43 +0200

    1. udělány enqueue a testy

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Node, Queue
+
+class testQueue(unittest.TestCase):
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(queue.head.data,'data1')
+        self.assertEqual(queue.head.next_node.data, "data2")
+        self.assertEqual(queue.tail.data, "data3")
+        self.assertIsNone(queue.tail.next_node)
